e001d4f4f9a9ab9108b26fe7ef4052a4c317bc9a
Aleksandrov Maksim <aleksandrovmagit@gmail.com>
2024-07-21 13:46:19 +0400

    Описание изменений: 	Настроил название h2 в application 	Подключил h2 и data-jpa 	Создал репозитории Order и Ingredient
    Новые классы:
    -	IngredientRepository
    -	OrderRepository
    Измененные классы:
    Удаленные классы:
    
    Файлы:
    pom, application

---
+++ src/main/java/com/example/TacoCloud/data/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.TacoCloud.data;
+
+import com.example.TacoCloud.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface OrderRepository
+        extends CrudRepository<TacoOrder, Long>{
+
+}
