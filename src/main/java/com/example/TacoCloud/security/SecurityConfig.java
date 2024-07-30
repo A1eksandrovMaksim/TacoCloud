72b4fcb23ac17b570886108d619c20fa4de16314
Aleksandrov Maksim <aleksandrovmagit@gmail.com>
2024-07-30 13:10:16 +0400

    csrf enable
    Новое:
    Изменено:
    	Во все формы html, кроме home, добавлен невидимый tag input csrf
    Удалено:

--- src/main/java/com/example/TacoCloud/security/SecurityConfig.java
+++ src/main/java/com/example/TacoCloud/security/SecurityConfig.java
@@ -1,20 +1,13 @@
 package com.example.TacoCloud.security;
 
 import com.example.TacoCloud.data.UserDataRepository;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -28,8 +21,6 @@ public class SecurityConfig {
             throws Exception{
         
         return http
-                .csrf(c -> c.disable())
-                .cors(c -> c.disable())
                 .authorizeHttpRequests(c -> {
                         c.requestMatchers("/design", "/orders").hasRole("USER");
                         c.requestMatchers("/", "/**").permitAll();
